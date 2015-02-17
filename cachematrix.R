a1e3291e7040b25eb18421c97de1bca2548bb971
avgijsel <anne.van.gijsel@knmi.nl>
2015-02-17 21:34:03 +0100

    working cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## This function creates a list of 4 functions that allow to read/set a matrix and its inverse
+  
+  c <- NULL # initialise variable c that will cache the inverse
+  setMatrix <- function(y) { # this function (re)sets the matrix and its inverse
+    x <<- y # reset matrix x to y
+    c <<- NULL # reset the inverse cache to null
+  }
+  makeCache <- function(solve) c <<- solve # this function will cache the inverse
+  getCache <- function() c # this function reads the cached inverse
+  getMatrix <- function() x # this function retrieves the data matrix
+  
+  list(setMatrix= setMatrix, makeCache = makeCache, # output of the main function is
+       getCache = getCache, getMatrix=getMatrix)    # a list with the four inner functions
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## This function returns a matrix that is the inverse of 'x'
+ 
+  mat <- x$getMatrix() # attempt to obtain the data matrix from the input x
+  if (!is.null(mat)) { # check if the data matrix is not null
+    cached<-x$getCache() # check if the inverse already exists
+    if(!is.null(cached)) { # inverse value is indeed stored, so return it and exit
+      return(cached) # returning the cached inverse
+    }
+    else { # inverse value is not stored, so compute the inverse, store it and return it
+      c <- solve(mat) # computing the inverse
+      x$makeCache(c) # caching the inverse
+      return(c) # returning the inverse
+    }
+  }
+  else {print("Incorrect input!")} # print an error message if the input x is faulty
 }
